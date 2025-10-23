d4ca0275be5d94d0bfd3e68b796e0ac71ca0b3f4
mamatha12-reddy <86341179+mamatha12-reddy@users.noreply.github.com>
2025-10-23 16:30:46 +0530

    Delete frontend directory

--- frontend/src/components/admin/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SubmitQuestionForm } from './SubmitQuestionForm';
-export { default as QuestionList } from './QuestionList';
-// Add other admin components here as they are created
